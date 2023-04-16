464dab2ed89f1beb6ac9fb2350c0237ab1ae7d58
aravindsak <aravindk.ark@gmail.com>
2023-04-16 19:23:58 +0530

    insert data

--- src/main/java/com/springboot/mongo/SpringmongoApplication.java
+++ src/main/java/com/springboot/mongo/SpringmongoApplication.java
@@ -1,7 +1,11 @@
 package com.springboot.mongo;
 
+import com.springboot.mongo.user.User;
+import com.springboot.mongo.user.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class SpringmongoApplication {
@@ -10,4 +14,20 @@ public class SpringmongoApplication {
 		SpringApplication.run(SpringmongoApplication.class, args);
 	}
 
+
+     @Bean
+    public CommandLineRunner commandLineRunner(
+            UserRepository repository
+
+    ) {
+        return args -> {
+            var user = User.builder()
+                    .name("Arun")
+                    .details("student")
+                    .build();
+            repository.insert(user);
+
+        };
+    }
 }
+
